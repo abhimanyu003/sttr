d5e27bf251b41a058746ac40627a7a2824bd0ffc
Abhimanyu Sharma <abhimanyusharma003@gmail.com>
2021-10-01 04:08:44 +0530

    Adding tests for markdown

---
+++ processors/markdown_test.go
@@ -0,0 +1,85 @@
+package processors
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestMarkdown_Command(t *testing.T) {
+	test := struct {
+		alias       []string
+		description string
+		filterValue string
+		flags       []Flag
+		name        string
+		title       string
+	}{
+		alias:       []string{"md-html"},
+		description: "Convert Markdown to HTML",
+		filterValue: "Markdown to HTML",
+		flags:       nil,
+		name:        "markdown-html",
+		title:       "Markdown to HTML",
+	}
+	p := Markdown{}
+	if got := p.Alias(); !reflect.DeepEqual(got, test.alias) {
+		t.Errorf("Alias() = %v, want %v", got, test.alias)
+	}
+	if got := p.Description(); got != test.description {
+		t.Errorf("Description() = %v, want %v", got, test.description)
+	}
+	if got := p.FilterValue(); got != test.filterValue {
+		t.Errorf("Flags() = %v, want %v", got, test.filterValue)
+	}
+	if got := p.Flags(); !reflect.DeepEqual(got, test.flags) {
+		t.Errorf("Flags() = %v, want %v", got, test.flags)
+	}
+	if got := p.Name(); got != test.name {
+		t.Errorf("Name() = %v, want %v", got, test.name)
+	}
+	if got := p.Title(); got != test.title {
+		t.Errorf("Title() = %v, want %v", got, test.title)
+	}
+}
+
+func TestMarkdown_Transform(t *testing.T) {
+	type args struct {
+		input string
+		in1   []Flag
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "test empty string",
+			args: args{input: ""},
+			want: "",
+		},
+		{
+			name: "test H1",
+			args: args{input: "# H1"},
+			want: "<h1>H1</h1>\n",
+		},
+		{
+			name: "test bold",
+			args: args{input: "**the quick brown fox jumps over a lazy dog**"},
+			want: "<p><strong>the quick brown fox jumps over a lazy dog</strong></p>\n",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := Markdown{}
+			got, err := p.Transform(tt.args.input, tt.args.in1...)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Transform() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Transform() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
