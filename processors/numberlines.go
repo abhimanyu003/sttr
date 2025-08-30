2ab9e9d7acfd63362ce38749a285da2d05546ec4
Kamila Chyla <230475+kamchy@users.noreply.github.com>
2025-08-30 07:15:43 +0200

    Add numberlines processor
    numberlines prepends non-empty lines with consecutive numbers

---
+++ processors/numberlines.go
@@ -0,0 +1,50 @@
+package processors
+
+import (
+	"fmt"
+	"strings"
+)
+
+// / Prepends consecutive number to each line of input
+type LineNumberer struct{}
+
+func (p LineNumberer) Name() string {
+	return "number-lines"
+}
+
+func (p LineNumberer) Alias() []string {
+	return []string{"nl", "number-lines"}
+}
+
+func (p LineNumberer) Transform(data []byte, _ ...Flag) (string, error) {
+	var s = string(data[:])
+	var counter = 1
+	var result = ""
+	var err error
+	for line := range strings.Lines(s) {
+		if line == "\n" {
+			result += line
+		} else {
+			result += fmt.Sprintf("%d. %s", counter, line)
+			counter += 1
+		}
+
+	}
+	return result, err
+}
+
+func (p LineNumberer) Flags() []Flag {
+	return nil
+}
+
+func (p LineNumberer) Title() string {
+	return "Line numberer"
+}
+
+func (p LineNumberer) Description() string {
+	return "Prepends consecutive number to each input line"
+}
+
+func (p LineNumberer) FilterValue() string {
+	return p.Title()
+}
